bbc49f63fc9e8ae31b6055ad7e64664f6a4aa554
Sherman25 <n.v.sherman@gmail.com>
2021-05-01 21:39:50 +0300

    guess game implementation

--- java/java-basic-course/src/main/java/model/Start.java
+++ java/java-basic-course/src/main/java/model/Start.java
@@ -1,17 +1,28 @@
 package model;
 
 import javax.swing.*;
+import java.util.Random;
 
 public class Start {
     public static void main(String[] args) {
-        int choice = 0;
+        Random random = new Random();
+        int target = random.nextInt(10);
+        int choice;
         while(true) {
             try {
                 choice = Integer.parseInt(JOptionPane.showInputDialog("input your choice"));
+                if(choice == target) {
+                    System.out.println(choice + " was correct");
+                    break;
+                } else if(choice < target) {
+                    System.out.println(choice + " is less than target");
+                } else {
+                    System.out.println(choice + " is greater than target");
+                }
             }catch (NumberFormatException e) {
                 e.printStackTrace();
             }
-            System.out.println(choice + " was correct");
+
         }
     }
 }
